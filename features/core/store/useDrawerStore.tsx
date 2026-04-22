09ebfcc2986e8c3d3b3761a7b3e1f5d178398604
mohammadmehdidabestani <mohammadmehdidabestani@chmail.ir>
2026-04-22 10:32:44 +0330

    change login url to root

--- features/core/store/useDrawerStore.tsx
+++ features/core/store/useDrawerStore.tsx
@@ -11,6 +11,5 @@ type DrawerState = {
 export const useDrawerStore = create<DrawerState>((set) => ({
   open: false,
   drawerWidth : 70,
-  
   setOpen: (open) => set({ open , drawerWidth : open ? DRAWER_WIDTH : MINI_DRAWER_WIDTH }),
 }));
