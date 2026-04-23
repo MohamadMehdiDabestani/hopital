787fb862687a046dce149bb21d30352f3955796d
mohammadmehdidabestani <mohammadmehdidabestani@chmail.ir>
2026-04-23 09:27:28 +0330

    Add util : create tehran time zone converter

--- features/core/index.ts
+++ features/core/index.ts
@@ -1,6 +1,7 @@
 export * from "./components";
 export * from "./actions";
 export * from "./store";
+export * from "./utils";
 // export * from './schemas';
 // export * from './forms';
 // export * from './queries';
