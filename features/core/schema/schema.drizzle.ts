788e0c2945b91e4393f3a24f5a86724c0dfa0cf0
mohammadmehdidabestani <mohammadmehdidabestani@chmail.ir>
2026-05-06 15:29:45 +0330

    new auth structure

--- features/core/schema/schema.drizzle.ts
+++ features/core/schema/schema.drizzle.ts
@@ -1,2 +1,1 @@
 export * from '@/features/auth/schemas/users.drizzle'
-export * from '@/features/auth/schemas/refreshToken.drizzle'
--- features/core/utils/jwt.ts
+++ features/core/utils/jwt.ts
@@ -1,27 +1,15 @@
+'use server'
 import { SignJWT, jwtVerify } from "jose";
-import crypto from 'crypto'
 const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
 export async function signAccessToken(payload: any) {
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("15m")
+    .setExpirationTime("12hrs")
     .sign(secret);
 }
-
-export async function signRefreshToken(payload: any) {
-  return await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("1d")
-    .sign(secret);
-}
-
 export async function verifyToken(token: string) {
   const { payload } = await jwtVerify(token, secret);
   return payload as { sub: string; role?: string };
-}
-export const hashToken = (token : string) => {
-  return crypto.createHash("sha256").update(token).digest("hex")
 }
